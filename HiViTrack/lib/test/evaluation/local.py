5ea09d30c99cd7c955da62b7bdb05b2107b74f53
huyj2001-ship-it <huyj2001@gmail.com>
2025-09-20 13:36:02 +0200

    Add files via upload

---
+++ HiViTrack/lib/test/evaluation/local.py
@@ -0,0 +1,30 @@
+
+from lib.test.evaluation.environment import EnvSettings
+
+def local_env_settings():
+    settings = EnvSettings()
+
+    # Set your local paths here.
+
+    settings.davis_dir = ''
+    settings.got10k_path = ''
+    settings.got_packed_results_path = ''
+    settings.got_reports_path = ''
+    settings.lasot_extension_subset_path = '/data2/wangyujie/ODTrack/data/lasot_extension_subset'
+    settings.lasot_path = '/data2/wangyujie/mixformer-wyj/MixFormer/data/lasot'
+    settings.network_path = '/data2/wangyujie/mixformer-wyj/MixFormer/lib/test/networks/'    # Where tracking networks are stored.
+    settings.nfs_path = ''
+    settings.otb_path = ''
+    settings.result_plot_path = '/data2/wangyujie/mixformer-wyj/MixFormer/test/result_plots/'
+    settings.results_path = '/data2/wangyujie/mixformer-wyj/MixFormer/test/tracking_results'    # Where to store tracking results
+    settings.segmentation_path = '/data2/wangyujie/mixformer-wyj/MixFormer/test/segmentation_results/'
+    settings.tn_packed_results_path = ''
+    settings.tpl_path = ''
+    settings.trackingnet_path = ''
+    settings.uav_path = ''
+    settings.vasttarck_path = ''
+    settings.vot_path = ''
+    settings.youtubevos_dir = ''
+
+    return settings
+
---
+++ HiViTrack/lib/test/tracker/t.py
@@ -0,0 +1,3 @@
+import torch
+torch.cuda.is_available()
+torch.cuda.device_count()
